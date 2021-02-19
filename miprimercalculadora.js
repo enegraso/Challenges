f3a48ee56962953cb29258aac7de2d7f31c98d0b
Federico Ortiz <sib2000@gmail.com>
2021-02-19 20:25:49 -0300

    com, creo yo, mejoras

--- miprimercalculadora.js
+++ miprimercalculadora.js
@@ -1,23 +1,36 @@
 num1 = prompt('Ingrese primer numero')
 num2 = prompt('Ingrese segundo numero')
-cuenta = prompt("Ingrese operador matematico \n + p/suma \n - p/resta" +
-                "\n * p/multiplicar \n / p/dividir")
+cuenta = prompt("Ingrese operador matemático \n Escriba + p/suma \n Escriba - p/resta" +
+                "\n Escriba * p/multiplicar \n Escriba / p/dividir \n Escriba p para potenciar" +
+                "\n Escriba r para raiz")
 num1 = parseInt(num1)
 num2 = parseInt(num2)
 
 switch (cuenta) {
-  case '+':
-    alert (num1 + num2)
-    break;
-  case '-':
-    alert (num1 - num2)
+    case '+':
+        var suma = num1 + num2
+        alert (num1 + " más " + num2 + " = " + suma)
         break;
-  case '*':
-    alert (num1 * num2)
+    case '-':
+        var resta = num1 - num2
+        alert (num1 + " menos " + num2 + " = " + resta)
         break;
-  case '/':
-    alert (num1 / num2)
+    case '*':
+        var multi = num1 * num2
+        alert (num1 + " por " + num2 + " = " + multi)
         break;
+    case '/':
+        var divid = num1 - num2
+        alert (num1 + " dividido " + num2 + " = " + divid)
+        break;
+    case 'p':
+        var poten = Math.pow(num1,num2)
+        alert (num1 + " elevado a " + num2 + " = " + poten)
+        break;
+    case 'r':
+        var raiz = Math.ceil(Math.pow(num1, 1 / num2))
+        alert (" raíz " + num2 + " de " + num1 + " = " + raiz)
+        break;    
     default:
-    alert("No se pudo hacer la operacion")
+        alert("No se pudo hacer la operacion")
 }
